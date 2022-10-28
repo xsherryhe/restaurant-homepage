39e492dbc96340de8feba43902cf201e4b08a613
Sherry He <xsherryhe@gmail.com>
2022-10-28 14:47:06 -0400

    Completed menu rendering module

--- README.md
+++ README.md
@@ -8,6 +8,49 @@ Image credits:
 
 1) RitaE: https://pixabay.com/users/ritae-19628/
 https://pixabay.com/photos/pancakes-yummy-dessert-4410606/
+https://pixabay.com/photos/pancakes-yummy-dessert-3926009/
 
 2) webandi: https://pixabay.com/users/webandi-1460261/
 https://pixabay.com/photos/pancakes-salmon-quark-pancake-rolls-2886046/
+
+3) GamerChef6: https://pixabay.com/users/gamerchef6-3307833/
+https://pixabay.com/photos/home-cooking-deviled-eggs-eggs-1670403/
+
+4) FoodieFactor: https://pixabay.com/users/foodiefactor-5992183/
+https://pixabay.com/photos/bacon-avocado-salad-lunch-food-2612198/
+
+5) BULBFISH: https://www.pexels.com/@bulbfish/
+https://www.pexels.com/photo/sliced-tomato-and-avocado-on-white-plate-1143754/
+
+6) congerdesign: https://pixabay.com/users/congerdesign-509903/
+https://pixabay.com/photos/pancakes-crepes-filled-pancake-577386/
+
+7) gaiagam: https://pixabay.com/users/gaiagam-2109922/
+https://pixabay.com/photos/aperitif-canap%c3%a9s-salmon-good-2611325/
+
+8) Александр Трубицын: https://www.pexels.com/@75310099/
+https://www.pexels.com/photo/close-up-shot-of-cooked-food-9082473/
+
+9) talhaicme97: https://pixabay.com/users/talhaicme97-18032277/
+https://pixabay.com/photos/crepe-strawberries-dessert-food-6003448/
+
+10) drawsandcooks: https://pixabay.com/users/drawsandcooks-6719143/
+https://pixabay.com/photos/crepe-banana-chocolate-eat-cute-2856552/
+
+11) Pexels: https://pixabay.com/users/pexels-2286921/
+https://pixabay.com/photos/bowl-french-fries-food-fries-1842294/
+
+12) bhumann34: https://pixabay.com/users/bhumann34-967924/
+https://pixabay.com/photos/food-spinach-artichoke-1179397/
+
+13) Eagle_: https://pixabay.com/users/eagle_-4928846/
+https://pixabay.com/photos/chicken-wings-secret-black-plates-2210461/
+
+14) alleksana: https://www.pexels.com/@alleksana/
+https://www.pexels.com/photo/close-up-shot-of-milkshakes-6463660/
+
+15) silviarita: https://pixabay.com/users/silviarita-3142410/
+https://pixabay.com/photos/smoothies-fruit-drink-food-2253423/
+
+16) fernandovillalobos: https://pixabay.com/users/fernandovillalobos-1265786/
+https://pixabay.com/photos/tea-cold-ice-2220485/
--- src/scripts/renderMenu.js
+++ src/scripts/renderMenu.js
@@ -2,13 +2,6 @@ import renderContentNone from './renderNone';
 import renderMenuItem from './renderMenuItem';
 import { contentElement } from './selectFromDOM';
 
-const menuSubheadingElements =
-  ['Appetizers', 'Crepes', 'Sides', 'Beverages'].map(text => {
-    const headingElement = document.createElement('h2');
-    headingElement.textContent = text;
-    return headingElement
-  });
-
 function renderMainHeading() {
   const menuHeadingElement = document.createElement('h1');
   menuHeadingElement.textContent = 'Menu';
@@ -16,35 +9,79 @@ function renderMainHeading() {
 }
 
 function renderAppetizers() {
-  const headingElement = document.createElement('h2'),
-        appetizersElement = document.createElement('div');
-  headingElement.textContent = 'Appetizers';
-  appetizerDetails().forEach(details => renderMenuItem(appetizersElement, ...details));
-  [headingElement, appetizersElement].forEach(element => contentElement.appendChild(element));
+  const appetizerDetails =
+    [['Salmon Rolls', 'Creamy bite-sized pieces of fresh salmon inside soft crepe wrap.',
+      require.context('../images/salmon-rolls/', false, /\.jpg$/i), 'Six salmon rolls'],
+    ['Deviled Eggs', 'Eggs loaded with yolk paste, cheese, aioli and seasoned with paprika and white vinegar.',
+      require.context('../images/deviled-eggs/', false, /\.jpg$/i), 'Plate of deviled eggs with yolk paste and paprika'],
+    ["Chef's Salad", 'Avocados, cucumber, sweet corn, tomatoes, and mushrooms tossed with your choice of dressing.',
+      require.context('../images/chefs-salad', false, /\.jpg$/i), 'Plate of salad with avocado and mushrooms']];
+
+  renderSection('Appetizers', appetizerDetails);
 }
 
-function appetizerDetails() {
-  return [['Salmon Rolls', 'Creamy bite-sized pieces of fresh salmon inside soft crepe wrap.',
-           require.context('../images/salmon-rolls/', false, /\.jpg$/i), 'Six salmon rolls']];
+function renderSavoryCrepes() {
+  const savoryCrepesDetails =
+    [['Classic Ham and Turkey Crepe', 'Ham, turkey, mushrooms, cheddar cheese, bell peppers, and melted butter',
+      require.context('../images/classic-crepe', false, /\.jpg$/i), 'Two wrapped crepes drizzled with butter sauce'],
+    ['Southwestern Crepe', 'Corn, tomatoes, black beans, bell peppers, sausage, and guacamole.',
+      require.context('../images/southwestern-crepe', false, /\.jpg$/i), 'Crepe topped with avocado, tomatoes, and corn'],
+    ['Pesto Chicken Crepe', 'Grilled chicken, mozzarella cheese, spinach, and pesto sauce.',
+      require.context('../images/chicken-pesto-crepe', false, /\.jpg$/i), 'Chicken, spinach, and cheese wrapped in crepe'],
+    ['Seaside Crepe', 'Smoked salmon, cream cheese, chives, spinach, and capers.',
+      require.context('../images/seaside-crepe', false, /\.jpg$/i), 'Crepes topped with folded salmon slices and herb']];
+
+  renderSection('Savory Crepes', savoryCrepesDetails);
 }
 
-function renderCrepes() {
+function renderSweetCrepes() {
+  const sweetCrepesDetails =
+    [["Chocolate Lover's Crepe", 'Nutella, dark chocolate chips, brownie filling, and strawberries topped with whipped cream, chocolate flakes, and chocolate syrup.',
+      require.context('../images/chocolate-lovers-crepe', false, /\.jpg$/i), 'Chocolate crepe topped with strawberries and chocolate syrup'],
+     ["Go Bananas Crepe", 'Bananas, banana pudding, peanut butter, and chopped walnuts topped with powdered sugar, chocolate syrup, and caramel syrup.',
+      require.context('../images/go-bananas-crepe', false, /\.jpg$/i), 'Bananas wrapped in crepe and topped with chocolate syrup'],
+     ["Berry Red Crepe", 'Strawberries, raspberries, blueberries, watermelon, and cream topped with powdered sugar and honey.',
+      require.context('../images/berry-red-crepe', false, /\.jpg$/i), 'Folded crepes topped with red berries']];
 
+  renderSection('Sweet Crepes', sweetCrepesDetails);
 }
 
 function renderSides() {
-
+  const sidesDetails = 
+    [['French Fries', 'Made from fresh potatoes, served with your choice of dipping sauce.',
+      require.context('../images/french-fries', false, /\.jpg$/i), 'Shoestring fries topped with green herb flakes'],
+     ['Artichoke Dip', 'Crafted from our special house recipe, served with your choice of tortilla chips or potato chips.',
+      require.context('../images/artichoke-dip', false, /\.jpg$/i), 'Bowl of artichoke dip surrounded by tortilla chips'],
+     ['Chicken Wings', 'Baked crispy and spicy wings served on a lettuce bed and with your choice of dipping sauce.',
+      require.context('../images/chicken-wings', false, /\.jpg$/i), 'Glazed chicken wings on a plate with lettuce']];
+  renderSection('Sides', sidesDetails);
 }
 
 function renderBeverages() {
+  const beveragesDetails =
+    [['Milkshake', 'Smooth and sweet, made with fresh whole milk. Available flavors: chocolate, vanilla, strawberry, banana.',
+      require.context('../images/milkshake', false, /\.jpg$/i), 'Four milkshakes of different flavors'],
+     ['Fruit Smoothie', 'Cool and refreshing. Choose from one or more of the following fruits: strawberry, watermelon, banana, pineapple, coconut, apple, blueberry, raspberry, pear, kiwi.',
+      require.context('../images/fruit-smoothies', false, /\.jpg$/i), 'Five colorful fruit smoothies surrounded by fruit'],
+     ['Iced Tea', 'Our brew is a customer favorite! Flavored with sugar and your choice of fruit.',
+      require.context('../images/iced-tea', false, /\.jpg$/i), 'Iced tea with lemon slices']];
+  renderSection('Beverages', beveragesDetails);
+}
 
+function renderSection(headingText, details) {
+  const headingElement = document.createElement('h2'),
+        detailsElement = document.createElement('div');
+  headingElement.textContent = headingText;
+  details.forEach(details => renderMenuItem(detailsElement, ...details));
+  [headingElement, detailsElement].forEach(element => contentElement.appendChild(element));
 }
 
 export default function renderMenu() {
   renderContentNone();
   renderMainHeading();
   renderAppetizers();
-  renderCrepes();
+  renderSavoryCrepes();
+  renderSweetCrepes();
   renderSides();
   renderBeverages();
 }
